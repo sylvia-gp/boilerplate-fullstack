7b50db9f07ca97b02ad83c70e89e66a811376b24
Sylvia Gilbert-Potts <51007051+sylvia-gp@users.noreply.github.com>
2019-09-10 15:30:44 +1200

    insert image tag into tank info

--- client/components/Tanks.jsx
+++ client/components/Tanks.jsx
@@ -8,29 +8,30 @@ export default class TankListing extends React.Component{
         super(props)
 
         this.state = {
-            tank: ''
+            tank: {}
         }
     }
 
     componentDidMount() {
         getTankById(this.props.match.params.id)
         .then(res => {
-            this.setState = {
-                tank: res.body
-            }
+            this.setState ({
+                tank: res.body[0]
+            })
         })
     }
 
     render() {
         return(
-            <div>
+            <React.Fragment>
             <h1>Welcome to tank {this.props.match.params.id}!</h1>
+            <img src={this.state.tank.img} height='200' width='200' />
             <ul>
             <li><Link to={`/fish/${this.props.match.params.id}`}>Check out your fish</Link></li>
             <li>Update your cleaning roster</li>
             <li>Wanna see your tank parameters?</li>
             </ul>
-            </div>
+            </React.Fragment>
         )
     }
 }
--- seeds/tanks.js
+++ seeds/tanks.js
@@ -7,8 +7,8 @@ exports.seed = function(knex, Promise) {
       return knex('tanks').insert([
         {id: 1, litres: '50', img:'image url goes here'},
         {id: 2, litres: '70', img: 'image url goes here'},
-        {id: 3, litres: '120', img: 'image url goes here'},
-        {id: 4, litres: '170', img: 'image url goes here'}
+        {id: 3, litres: '120', img: 'https://www.redwoodaquatics.co.nz/wp-content/uploads/2017/08/product_1067.jpg'},
+        {id: 4, litres: '170', img: 'http://www.aquarium-site.com/wp-content/uploads/2019/01/tank-aquariums-of-aqua-one-silver-ufo-700-corner-fish-675.jpg'}
       ]);
     });
 };
