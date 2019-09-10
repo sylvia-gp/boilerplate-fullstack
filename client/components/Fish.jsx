393f61c9e8029f043f40ee22d45263acc70c31fb
Sylvia Gilbert-Potts <51007051+sylvia-gp@users.noreply.github.com>
2019-09-10 16:04:34 +1200

    get images displaying for fish

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -16,7 +16,7 @@ export default class FishListing extends React.Component {
   componentDidMount() {
     getFishByTankId(this.props.match.params.id)
       .then(res => {
-        this.setState ({
+        this.setState({
           fishes: res.body
         })
       })
@@ -28,7 +28,7 @@ export default class FishListing extends React.Component {
         <h1>Here are some fish</h1>
         <ul>
           {this.state.fishes.map(fish => {
-            return <li key={fish.id}>Fish: {fish.species} - Number in tank: {fish.quantity} - Image: {fish.image}</li>
+            return <li key={fish.id}><img src={fish.image} height='200' /><p>Fish: {fish.species} - Number in tank: {fish.quantity}</p></li>
           })}
         </ul>
       </React.Fragment>
--- seeds/fish.js
+++ seeds/fish.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('fish').insert([
-        {id: 1, image: '', species: 'Angelfish', quantity: '1', tank_id: '4'},
-        {id: 2, image: 'sparkling-gourami.jpg', species: 'Sparkling Gourami', quantity: '6', tank_id: '3'},
-        {id: 3, image: 'pea-puffer.jpg', species: 'Pea Puffers', quantity: '3', tank_id: '2'},
-        {id: 4, image: 'pygmy-cory.jpg', species: 'Pygmy Corys', quantity: '8', tank_id: '1'},
-        {id: 5, image: 'albino-bristlenose.jpg', species: 'Albino Long-finned Bristlenoses', quantity: '2', tank_id: '4'},
-        {id: 6, image: 'rio-salinas.jpg', species: 'Rio Salinas Corys', quantity: '8', tank_id: '3'},
-        {id: 7, image: 'black-orchid-betta.jpg', species: 'Black Orchid Crowntail Betta', quantity: '1', tank_id: '1'},
+        {id: 1, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSXwbZdKBYkZVPCVndfQRQvRCBjmAlkDdjUWAP2T6GzwEoyTFxI', species: 'Angelfish', quantity: '1', tank_id: '4'},
+        {id: 2, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfhRe2vil3SVWmqP64V4fXJndGfoeOfV7b5HRj6ggnXJwgp5ls', species: 'Sparkling Gourami', quantity: '6', tank_id: '3'},
+        {id: 3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTu113AjV5UZKRpkroezVEwuGeMpRLJhP2_Boamx3rfFYsQcs2v', species: 'Pea Puffers', quantity: '3', tank_id: '2'},
+        {id: 4, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgFPO216_ahZOcdVvXCzPwyVQNdwLimzWVx5qZ6eDm-8yNdolV', species: 'Pygmy Corys', quantity: '8', tank_id: '1'},
+        {id: 5, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTYMMf4XmxGTfCvQjxsh8dNJr7WzhTWhfqyxBFQ6PwNnji21E79', species: 'Albino Long-finned Bristlenoses', quantity: '2', tank_id: '4'},
+        {id: 6, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRHC3jQOgQogh-jOm6knAkVlSFiPL9_OFzbaXiAuxHSf6J1qdcT', species: 'Rio Salinas Corys', quantity: '8', tank_id: '3'},
+        {id: 7, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQon6HBu13BrQKyf9YUW4_wLzVdQBURcNAr1LSCv6J_bjelc0h1', species: 'Black Orchid Crowntail Betta', quantity: '1', tank_id: '1'},
       ]);
     });
 };
