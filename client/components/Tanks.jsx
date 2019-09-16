ae0415e074ae8f9b7e12d0fb1f7d6c7b495e077b
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-09-16 21:31:33 +1200

    add tank image urls

--- client/components/Tanks.jsx
+++ client/components/Tanks.jsx
@@ -25,7 +25,7 @@ export default class TankListing extends React.Component {
         return (
             <React.Fragment>
                 <h2>Welcome to tank {this.props.match.params.id}!</h2>
-                <img src={this.state.tank.img} height='200' width='200' />
+                <img src={this.state.tank.img} height='250' />
                 <ul>
                     <li><Link to={`/fish/${this.props.match.params.id}`}>Check out your fish</Link></li>
                     <li><Link to={`/tanks/${this.props.match.params.id}/cleaning`}>Update your cleaning roster</ Link></li>
--- seeds/tanks.js
+++ seeds/tanks.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('tanks').insert([
-        {id: 1, litres: '50', img:'image url goes here'},
-        {id: 2, litres: '70', img: 'image url goes here'},
-        {id: 3, litres: '120', img: 'https://www.redwoodaquatics.co.nz/wp-content/uploads/2017/08/product_1067.jpg'},
-        {id: 4, litres: '170', img: 'http://www.aquarium-site.com/wp-content/uploads/2019/01/tank-aquariums-of-aqua-one-silver-ufo-700-corner-fish-675.jpg'}
+        {id: 1, litres: '50', img:'https://files.slack.com/files-pri/T02SQPVAC-FND3Q5E6S/20190219_175624.jpg'},
+        {id: 2, litres: '70', img: 'https://files.slack.com/files-pri/T02SQPVAC-FNASZNNQM/20190325_131044.jpg'},
+        {id: 3, litres: '120', img: 'https://files.slack.com/files-pri/T02SQPVAC-FNCNTUHBR/20190910_200934.jpg'},
+        {id: 4, litres: '170', img: 'https://files.slack.com/files-pri/T02SQPVAC-FNAT0JWSZ/20190910_200907.jpg'}
       ]);
     });
 };
